97e017d169ae2b5961bf0fe2fe8c6c20011e68cb
Maryam Moeed <125427712+maryammoeed@users.noreply.github.com>
2024-12-13 15:03:15 +0500

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 8501;
+const PORT = process.env.PORT || 8000;
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
